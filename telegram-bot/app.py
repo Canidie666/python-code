d85d34c8be43d2cfe762e9e99ca02e7c2337e9f0
Canidie666 <yolopuphimom@gmail.com>
2023-07-24 23:36:14 +0300

    telegram-bot

---
+++ telegram-bot/app.py
@@ -0,0 +1,53 @@
+import telebot
+from telebot import types
+
+bot = telebot.TeleBot('6386618240:AAHeV7dFpv1NPHzR1N2Aa1UX9TIEt6woFC4')
+
+name = ''
+surename = ''
+age = 0 
+
+@bot.message_handler(content_types=['text'])
+def startReg(message):
+    if message.text == "/reg":
+        bot.send_message(message.from_user.id, "Type your name")
+        bot.register_next_step_handler(message, getName)  
+    else:
+        bot.send_message(message.from_user.id, "Type /reg")
+
+def getName(message):
+    global name
+    name = message.text
+    bot.send_message(message.from_user.id, "Type your sure name")
+    bot.register_next_step_handler(message, getSureName) 
+
+def getSureName(message):
+    global surename
+    surename = message.text
+    bot.send_message(message.from_user.id, "Type your age")
+    bot.register_next_step_handler(message, getAge)
+
+def getAge(message):
+    global age 
+    while age == 0:
+        try:
+            age = int(message.text)
+        except Exception:
+            bot.send_message(message.from_user.id, "Input must be integer")
+        keyboard = types.InlineKeyboardMarkup()
+        keyYes = types.InlineKeyboardButton(text="Yes", callback_data="yes")
+        keyboard.add(keyYes)
+        keyNo = types.InlineKeyboardButton(text="No", callback_data="no")
+        keyboard.add(keyNo)
+        question = "Your age " + str(age) + ", your name " + str(name)+ " " + str(surename)
+        bot.send_message(message.from_user.id, text=question, reply_markup=keyboard) 
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_worker(call):
+    if call.data == "yes":
+        bot.send_message(call.message.chat.id, 'Success')
+    elif call.data == "no":  
+        bot.send_message(call.message.chat.id, "Type /reg if you want to repeat registration.") 
+        
+
+bot.polling(none_stop=True, interval=0)
